4752c9c631f15a867e8a2aad701ea3dfad27fbc3
Risadeiro <malcolm.osoares@gmail.com>
2021-12-03 00:45:17 -0300

    Exemplo de teste

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,36 +1,35 @@
 // import React from "react";
-import {
-  render
-} from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import {render} from "@testing-library/react";
+import {MemoryRouter} from "react-router-dom";
 import UserForm from "./components/UserForm";
 import "@testing-library/jest-dom/extend-expect";
-import { act } from 'react-dom/test-utils';
+import {act} from "react-dom/test-utils";
 
 test("renders UserForm", () => {
-  const {getByText} = render(    <MemoryRouter initialEntries={["/fpc/2"]}>
-  <UserForm />
-</MemoryRouter>
-)
-  const linkElement = getByText(/Preencha a seguir o formulário pré-consulta/i)
+  const {getByText} = render(
+    <MemoryRouter initialEntries={["/fpc/2"]}>
+      <UserForm />
+    </MemoryRouter>
+  );
+  const linkElement = getByText(/Preencha a seguir o formulário pré-consulta/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test("navigates home when you click the logo", () => {
   // in a real test a renderer like "@testing-library/react"
   // would take care of setting up the DOM elements
-  render(<MemoryRouter initialEntries={["/fpc/2"]}>
-  <UserForm />
-</MemoryRouter>
-)
-
+  render(
+    <MemoryRouter initialEntries={["/fpc/2"]}>
+      <UserForm />
+    </MemoryRouter>
+  );
 
   // Interact with page
   act(() => {
     // PRIMEIRA PAGINA DE PRE CONSULTA
     const goHomeLink = document.querySelector(".MuiButton-root");
     // Click it
-    goHomeLink.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    goHomeLink.dispatchEvent(new MouseEvent("click", {bubbles: true}));
   });
   // Check correct page content showed up
   expect(document.body.textContent).toContain("Pré Consulta");
@@ -39,7 +38,7 @@ test("navigates home when you click the logo", () => {
     // SEGUNDA PAGINA DE PRE CONSULTA
     const goHomeLink = document.querySelector(".MuiButton-root");
     // Click it
-    goHomeLink.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    goHomeLink.dispatchEvent(new MouseEvent("click", {bubbles: true}));
   });
   expect(document.body.textContent).toContain("Permaneça parado por pelo");
 
@@ -47,7 +46,7 @@ test("navigates home when you click the logo", () => {
     // nOS CONTE SOBRE VOCE
     const goHomeLink = document.querySelector(".MuiButton-containedPrimary");
     // Click it
-    goHomeLink.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    goHomeLink.dispatchEvent(new MouseEvent("click", {bubbles: true}));
   });
   expect(document.body.textContent).toContain("Nos conte sobre");
   expect(document.body.textContent).not.toContain("Favor");
@@ -55,8 +54,7 @@ test("navigates home when you click the logo", () => {
     // nOS CONTE SOBRE VOCE
     const goHomeLink = document.querySelector(".MuiButton-containedPrimary");
     // Click it
-    goHomeLink.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    goHomeLink.dispatchEvent(new MouseEvent("click", {bubbles: true}));
   });
   expect(document.body.textContent).not.toContain("Favor Preencher");
-
 });
